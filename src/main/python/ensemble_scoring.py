344f78175585385d5fec716fe060be5c814ee32c
elliot-j <elliot.c.justice@gmail.com>
2023-04-27 20:17:28 -0700

    adding ndcg to ensemble scoring

--- src/main/python/ensemble_scoring.py
+++ src/main/python/ensemble_scoring.py
@@ -1,5 +1,7 @@
+from difflib import restore
 import json
 from collections import defaultdict
+import math
 
 
 def parse_file(file_path):
@@ -44,20 +46,22 @@ def calculate_ensemble_score(entries_lucene, entries_bm25, lambda_value=36.1):
     for entry in entries_lucene:
         doc_id = entry['docId']
         score = entry['score']
-        scores[doc_id] = {'lucene': score, 'bm25': 0}
+        userScore = entry['userScore']
+        scores[doc_id] = {'lucene': score, 'bm25': 0, 'userScore':userScore}
 
     for entry in entries_bm25:
         doc_id = entry['docId']
         score = entry['score']
+        userScore = entry['userScore']
         if doc_id in scores:
             scores[doc_id]['bm25'] = score
         else:
-            scores[doc_id] = {'lucene': 0, 'bm25': score}
+            scores[doc_id] = {'lucene': 0, 'bm25': score,'userScore':userScore}
 
     ensemble_scores = {}
     for doc_id, score_data in scores.items():
         ensemble_score = score_data['lucene'] + (lambda_value * score_data['bm25'])
-        ensemble_scores[doc_id] = ensemble_score
+        ensemble_scores[doc_id] = {'ensemble': ensemble_score, 'userScore':score_data['userScore']}
     return ensemble_scores
 
 
@@ -78,7 +82,7 @@ def get_min_max_ensemble_scores(ensemble_scores_by_query):
     :param ensemble_scores_by_query: the final ensemble_scores_by_query
     :return:
     """
-    all_scores = [entry["ensembleScore"] for query_results in ensemble_scores_by_query.values() for entry in
+    all_scores = [entry["ensemble"] for query_results in ensemble_scores_by_query.values() for entry in
                   query_results]
     min_score = min(all_scores)
     max_score = max(all_scores)
@@ -106,20 +110,44 @@ def calculate_mrr(ensemble_scores_by_query, golden_documents):
             reciprocal_ranks.append(0)
     mrr = sum(reciprocal_ranks) / len(reciprocal_ranks)
     return mrr
+def computeNormalizedGain(data):
+	dgc =0
+	for i, value in enumerate(data):
+		val = value['userScore']
+		pos = i + 1
+		dgc +=  val / math.log(pos+1, 2)
+	
+	sortedData = sorted(data, key= lambda x : x['userScore'], reverse = True)
 
+	idgc =0
+	for i, value in enumerate(sortedData):
+		val = value['userScore']
+		pos = i + 1
+		idgc +=  val / math.log(pos+1, 2)
+	if idgc == 0:
+		return 0 
+	return dgc/idgc
+def computeGainForQuery(queryResults):
+    
+    results = []
+    for i in range(0, 9) :
+        results.append(queryResults[i])
+    return computeNormalizedGain(results)
 
 if __name__ == "__main__":
-    data_file_lucene = "lucene-results-bm25.json"
-    data_file_bm25 = "complete-annoy-results-query-name-combined.json"
+    data_file_lucene = "results/lucene-results-bm25v2.json"
+    data_file_bm25 = "results/complete-annoy-results-transformer.json"
     data_lucene = parse_file(data_file_lucene)
     data_bm25 = parse_file(data_file_bm25)
     grouped_data = group_by_query(data_lucene, data_bm25)
     ensemble_scores_by_query = {}
+    nGain = []
     for query, entries in grouped_data.items():
         ensemble_scores = calculate_ensemble_score(entries['lucene'], entries['bm25'], lambda_value = 36.1)
-        sorted_results = sorted(ensemble_scores.items(), key=lambda x: x[1], reverse=True)
-        sorted_results_json = [{"docId": doc_id, "ensembleScore": score} for doc_id, score in sorted_results]
+        sorted_results = sorted(ensemble_scores.items(), key=lambda x: x[1]['ensemble'], reverse=True)
+        sorted_results_json = [{"docId": key, "ensemble": item['ensemble'], 'userScore':item['userScore']} for key, item in sorted_results]
         ensemble_scores_by_query[query] = sorted_results_json
+        nGain.append(computeGainForQuery(sorted_results_json))
     print(f"[min, max] ensemble score = {get_min_max_ensemble_scores(ensemble_scores_by_query)}")
     golden_documents = ["133869", "1945581", "1027458", "1218574", "205807", "1046961", "1410596", "723621", "2154967",
                         "480172", "12151555", "517193", "1790898", "361214", "1636233", "468086", "1118514", "1255935",
@@ -127,5 +155,11 @@ if __name__ == "__main__":
                         "1174207", "1584628", "521466", "2205489"]
     mrr = calculate_mrr(ensemble_scores_by_query, golden_documents)
     print("Mean Reciprocal Rank (MRR) using the ensemble method:", mrr)
-    output_file = "sorted_ensemble_scores_by_query.json"
+    output_file = "results/sorted_ensemble_scores_by_query.json"
     save_sorted_results_to_json(ensemble_scores_by_query, output_file)
+    
+    sum = 0
+    for i in nGain:
+        sum+= i
+        print( round(i, 6))
+    print (f"average: { round(sum/31, 6)}")
